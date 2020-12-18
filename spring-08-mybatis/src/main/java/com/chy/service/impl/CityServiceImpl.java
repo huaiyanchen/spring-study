66bc89e6e81db4ca5fe30a63d79d3bf33eb9ea7c
chy <18712302931@163.com>
2020-12-18 15:31:17 +0800

    spring-mybatis初步整合方法二

--- spring-08-mybatis/src/main/java/com/chy/service/impl/CityServiceImpl.java
+++ spring-08-mybatis/src/main/java/com/chy/service/impl/CityServiceImpl.java
@@ -3,19 +3,20 @@ package com.chy.service.impl;
 import com.chy.Mapper.CityMapper;
 import com.chy.dao.City;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import java.util.List;
 
-public class CityServiceImpl implements CityMapper {
+public class CityServiceImpl extends SqlSessionDaoSupport implements CityMapper {
 
-    private SqlSessionTemplate sqlSession;
-
-    public void setSqlSession(SqlSessionTemplate sqlSession) {
-        this.sqlSession = sqlSession;
-    }
+//    private SqlSessionTemplate sqlSession;
+//
+//    public void setSqlSession(SqlSessionTemplate sqlSession) {
+//        this.sqlSession = sqlSession;
+//    }
 
     @Override
     public List<City> selectAll() {
-        return sqlSession.getMapper(CityMapper.class).selectAll();
+        return getSqlSession().getMapper(CityMapper.class).selectAll();
     }
 }
